a2500af40e81546ff28ab32c634ec9916ded2496
Brian Lobo <brian.d.lobo@gmail.com>
2020-01-07 17:44:43 -0300

    arreglo scripts

--- scripts/heroku/despliegue.sh
+++ scripts/heroku/despliegue.sh
@@ -4,7 +4,7 @@ source ./scripts/docker/ambiente.sh
 cd ./scripts/heroku
 
 # echo $(heroku auth:token) | docker login -u $USER --password-stdin registry.heroku.com
-echo "$DOCKER_TOKEN" | docker login --username $DOCKER_USER --password-stdin registry.heroku.com
+echo "$HEROKU_API_KEY" | docker login --username $DOCKER_USER --password-stdin registry.heroku.com
 
 heroku container:login
 heroku container:push web -a $HEROKU_APP --arg TAG=$DOCKER_TAG
