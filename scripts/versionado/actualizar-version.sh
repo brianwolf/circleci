829b4e25563c46a05456e0201904e87ed081d593
Brian Lobo <brian.d.lobo@gmail.com>
2020-01-07 15:13:21 -0300

    Auto stash before rebase of "v2"

--- scripts/versionado/actualizar-version.sh
+++ scripts/versionado/actualizar-version.sh
@@ -1,7 +1,12 @@
 source ./scripts/versionado/ambiente.sh
 
-a=2
+# a=2
+
+
+# echo "$(($a + 1))"
 
 echo $(git tag)
 
-echo "$(($a + 1))"
+IFS='v' read -r -a array <<< "$(git tag)"
+
+echo ${array[2]}
