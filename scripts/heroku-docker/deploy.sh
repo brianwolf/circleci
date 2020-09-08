2eeb3d4f3e29ffe11b6e4f81d497a5cbace67e4f
Brian Lobo <brian.lobo@moorea.io>
2020-09-08 13:45:55 -0300

    maldicion

--- scripts/heroku-docker/deploy.sh
+++ scripts/heroku-docker/deploy.sh
@@ -2,7 +2,7 @@
 
 echo "$HEROKU_API_KEY" | docker login --username $HEROKU_EMAIL --password-stdin $HEROKU_DOCKER_REGISTRY 
 
-heroku create $HEROKU_APP
+heroku create $HEROKU_APP || echo "Created $HEROKU_APP" 
 
 heroku container:login
 heroku container:push web -a $HEROKU_APP
